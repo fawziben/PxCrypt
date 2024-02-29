61cb08cb1a79637c3c97bba48504addf8c99db3e
hp <if_benmoumen@esi.dz>
2024-02-29 23:28:02 +0100

    add analytic charts

--- src/Components/FileStats.js
+++ src/Components/FileStats.js
@@ -1,44 +1,105 @@
-import { Doughnut, Pie } from "react-chartjs-2";
-import { Chart, ArcElement, Tooltip, Legend } from "chart.js";
-Chart.register(ArcElement)
+import React, { useEffect, useState } from "react";
+import { Chart as ChartJS, defaults } from "chart.js/auto";
+import { Bar, Doughnut, Line } from "react-chartjs-2";
+import {Chart, ArcElement} from 'chart.js'
+
+import "./FileStats.css";
+
+import sourceData from "./data/sourceData.json";
+
+Chart.register(ArcElement);
 
 
-const FileStats = () => {
-  const data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [
-      {
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          'red',
-          'blue',
-          'yellow',
-          'green',
-          'purple',
-          'orange',
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
+defaults.maintainAspectRatio = false;
+defaults.responsive = true;
 
-  const options = {
-    plugins: {
-      legend: {
-        display: true,
-        position: 'left',
-        responsive: true,
+defaults.plugins.title.display = true;
+defaults.plugins.title.align = "start";
+defaults.plugins.title.font.size = 20;
+defaults.plugins.title.color = "black";
 
-      },
-    },
-  };
+export default function FileStats  ()  {
+    const [gap, setGap] = useState(window.innerWidth <= 1200 ? "1vw" : "4vw");
+const [width, setWidth] = useState(window.innerWidth <= 1200 ? "81%" : "85%");
+
+
+  useEffect(() => {
+    const handleResize = () => {
+      setGap(window.innerWidth <= 1200 ? "1vw" : "4vw");
+      setWidth(window.innerWidth <= 1200 ? "81%" : "85%");
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
-    <div sx={{backgroundColor : '#ffffff', display : 'flex'}}>
-      <Doughnut data={data} options={options} />
+    <div className="App" style={{gap}}>
+      <div className="dataCard categoryCard">
+        <Doughnut
+          data={{
+            labels: sourceData.map((data) => data.label),
+            datasets: [
+              {
+                label: "Count",
+                data: sourceData.map((data) => data.value),
+                backgroundColor: [
+                  "rgba(43, 63, 229, 0.8)",
+                  "rgba(250, 192, 19, 0.8)",
+                  "rgba(253, 135, 135, 0.8)",
+                ],
+                borderColor: [
+                  "rgba(43, 63, 229, 0.8)",
+                  "rgba(250, 192, 19, 0.8)",
+                  "rgba(253, 135, 135, 0.8)",
+                ],
+              },
+            ],
+          }}
+          options={{
+            plugins: {
+              title: {
+                text: "Repartition des fichiers cryptes sur le serveur",
+              },
+            },
+          }}
+        />
+      </div>
+      <div className="dataCard categoryCard">
+        <Doughnut
+          data={{
+            labels: sourceData.map((data) => data.label),
+            datasets: [
+              {
+                label: "Count",
+                data: sourceData.map((data) => data.value),
+                backgroundColor: [
+                  "rgba(43, 63, 229, 0.8)",
+                  "rgba(250, 192, 19, 0.8)",
+                  "rgba(253, 135, 135, 0.8)",
+                ],
+                borderColor: [
+                  "rgba(43, 63, 229, 0.8)",
+                  "rgba(250, 192, 19, 0.8)",
+                  "rgba(253, 135, 135, 0.8)",
+                ],
+              },
+            ],
+          }}
+          options={{
+            plugins: {
+              title: {
+                text: "Repartition des fichiers cryptes",
+              },
+            },
+          }}
+        />
+      </div>
+      <div className="dataCard revenueCard" style={{width}}></div>
     </div>
+
   );
 };
-
-export default FileStats;
